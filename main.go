ebced537f37c310e02d1397fd39a544ca344fb33
ryoukata <ryoya3091@gmail.com>
2021-05-03 17:57:06 +0900

    feat(#7): add function to get String Choices for Twitter Votes.

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+)
+
+// connect and close settings for MongoDB.
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("dialing to MongoDB...: localhost")
+	db, err = mgo.Dial("localhost")
+
+	return err
+}
+func closedb() {
+	db.Close()
+	log.Println("Closed Connection for MongoDB.")
+}
+
+// get all selection Strings to use poll.
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+
+	return options, iter.Err()
+}
+
 func main() {
 
 }
